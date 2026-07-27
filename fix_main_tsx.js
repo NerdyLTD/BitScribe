95a4e68068debb5522f004068ad7a3ab2900113b
AI Studio Agent <ubsjasongw@gmail.com>
2026-07-27 06:31:16 +0000

    chore: cleanup patch scripts

--- fix_main_tsx.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let file = fs.readFileSync('apps/steward/src/main.tsx', 'utf8');
-file = file.replace(/\\n\\ncreateRoot/g, '\\n\\ncreateRoot');
-fs.writeFileSync('apps/steward/src/main.tsx', file);
--- patch_core.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let file = fs.readFileSync('packages/core-types/index.ts', 'utf8');
-file = file.replace('  useCleanNonLatinTags?: boolean;\\n  bleedingEdgeVideoCodecs?: string[];', '  useCleanNonLatinTags?: boolean;\\n  diagnosticLoggingEnabled?: boolean;\\n  bleedingEdgeVideoCodecs?: string[];');
-fs.writeFileSync('packages/core-types/index.ts', file);
